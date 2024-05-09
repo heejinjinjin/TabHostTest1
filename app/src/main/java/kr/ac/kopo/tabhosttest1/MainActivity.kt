9245d8382ebc3560b367187399464b69568a6339
heejinjinjin <sshj2917@gmail.com>
2024-05-09 15:52:53 +0900

    TabHost 첫번째 방법

---
+++ app/src/main/java/kr/ac/kopo/tabhosttest1/MainActivity.kt
@@ -0,0 +1,33 @@
+package kr.ac.kopo.tabhosttest1
+
+import android.app.TabActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : TabActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var tabHost = this.tabHost
+
+        var tabSpec1 = tabHost.newTabSpec("first").setIndicator("첫 번째 탭")
+        tabSpec1.setContent(R.id.gyodong)
+        tabHost.addTab(tabSpec1)
+
+        var tabSpec2 = tabHost.newTabSpec("second").setIndicator("두 번째 탭")
+        tabSpec2.setContent(R.id.gyodong2)
+        tabHost.addTab(tabSpec2)
+
+        var tabSpec3 = tabHost.newTabSpec("third").setIndicator("세 번째 탭")
+        tabSpec3.setContent(R.id.gyodong3)
+        tabHost.addTab(tabSpec3)
+
+        var tabSpec4 = tabHost.newTabSpec("fourth").setIndicator("네 번째 탭")
+        tabSpec4.setContent(R.id.gyodong4)
+        tabHost.addTab(tabSpec4)
+
+        tabHost.currentTab = 1
+    }
+}
